8a83a4008b4bab78da027197da791b9dc4fe9165
Tony Ramirez <tonyram20@gmail.com>
2016-08-09 11:20:11 -0400

    Added github link

--- js/app.js
+++ js/app.js
@@ -1,77 +1,81 @@
 var app = angular.module("home", []);
 
 app.directive("topSection", function() {
-  return {
-    restrict: 'E',
-    templateUrl: '../views/top-section.html',
-    controller: function() {
-      this.isInContactsPage = false;
-      this.goToContacts = function() {
-        this.isInContactsPage = true;
-      };
+    return {
+        restrict: 'E',
+        templateUrl: '../views/top-section.html',
+        controller: function() {
+            this.isInContactsPage = false;
+            this.goToContacts = function() {
+                this.isInContactsPage = true;
+            };
 
-      this.goToGames = function() {
-        this.isInContactsPage = false;
-      };
-    },
-    controllerAs: 'topSection'
-  };
+            this.goToGames = function() {
+                this.isInContactsPage = false;
+            };
+        },
+        controllerAs: 'topSection'
+    };
 });
 
 app.directive("contactSection", function() {
-  return {
-    restrict: 'E',
-    templateUrl: '../views/contact-section.html',
-    controller: function() {
-      var contact = [
-        {
-          name: "LinkedIn",
-          url: "https://ve.linkedin.com/in/ramireztony"
+    return {
+        restrict: 'E',
+        templateUrl: '../views/contact-section.html',
+        controller: function() {
+            var contact = [
+                {
+                    name: "LinkedIn",
+                    url: "https://ve.linkedin.com/in/ramireztony"
+                },
+                {
+                    name: "Twitter",
+                    url: "https://twitter.com/tonyram19"
+                },
+                {
+                    name: "Bitbucket",
+                    url: "http://bitbucket.com/tonyram19"
+                },
+                {
+                    name: "Github",
+                    url: "http://github.com/tonyram19"
+                },
+                {
+                    name: "tonyram20@gmail.com",
+                    url: "mailto:tonyram20@gmail.com"
+                }
+            ];
+            this.sites = contact;
         },
-        {
-          name: "Twitter",
-          url: "https://twitter.com/tonyram19"
-        },
-        {
-          name: "Bitbucket",
-          url: "http://bitbucket.com/tonyram19"
-        },
-        {
-          name: "tonyram20@gmail.com",
-          url: "mailto:tonyram20@gmail.com"
-        }
-      ];
-      this.sites = contact;
-    },
-    controllerAs: 'contactSection'
-  };
+        controllerAs: 'contactSection'
+    };
 });
 
 app.directive("gamesSection", function() {
-  return {
-    restrict: 'E',
-    templateUrl: '../views/games-section.html',
-    controller: function() {
-      var gamesArray = [
-        {
-          name: "Isowood",
-          imgUrl: "../img/isowood.png",
-          gameUrl: "https://play.google.com/store/apps/details?id=com.ramirezTony.Isowood&hl=en",
-        },
-        {
-          name: "MVSC",
-          imgUrl: "../img/mvsc.png",
-          gameUrl: "https://play.google.com/store/apps/details?id=com.ramiezTony.Case&hl=en",
-        },
-        {
-          name: "Zombie: The Movie",
-          imgUrl: "../img/shooter.jpg",
-          gameUrl: "#",
-        }
-      ];
+    return {
+        restrict: 'E',
+        templateUrl: '../views/games-section.html',
+        controller: function() {
+            var gamesArray = [
+                {
+                    name: "Isowood",
+                    imgUrl: "../img/isowood.png",
+                    gameUrl: "https://play.google.com/store/apps/details?id=com.ramirezTony.Isowood&hl=en",
+                },
+                {
+                    name: "MVSC",
+                    imgUrl: "../img/mvsc.png",
+                    gameUrl: "https://play.google.com/store/apps/details?id=com.ramiezTony.Case&hl=en",
+                },
+                {
+                    name: "Zombie: The Movie",
+                    imgUrl: "../img/shooter.jpg",
+                    gameUrl: "#",
+                }
+            ];
 
-      this.games = gamesArray;
-    },
-    controllerAs: 'gamesSection'
-  };
+            this.games = gamesArray;
+        },
+        controllerAs: 'gamesSection'
+    };
 });
